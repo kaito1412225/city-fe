df0fcd1f6f0f7882d9fb4563f3402f745516aeda
lehoangdt9 <lehoangdt9@gmail.com>
2022-08-25 10:05:26 +0700

    ++ formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { SignIn } from "./features/auth/pages/SignIn";
+import ForgotPassword from "./features/auth/pages/ForgotPassword";
+import { Authentication } from "./features/auth";
+import ResetPassword from "./features/auth/pages/ResetPassword";
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Authentication />}>
+          <Route index element={<SignIn />} />
+          <Route path="email-reset" element={<ForgotPassword />} />
+          <Route path="password-reset" element={<ResetPassword />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
