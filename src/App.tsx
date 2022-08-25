2216133493940d99f175829faaca3b9e6c930f56
kaito1412225 <63288373+kaito1412225@users.noreply.github.com>
2022-08-25 10:09:42 +0700

    login fe

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { SignIn } from "./features/auth/pages/SignIn";
+import { StyledEngineProvider } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <StyledEngineProvider injectFirst>
+        <SignIn />
+      </StyledEngineProvider>
     </div>
   );
 }
